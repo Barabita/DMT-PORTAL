4016ab41057262577be29aa8e7204f390b900e68
malisahin <mehmetalisahinogullari@gmail.com>
2018-03-13 02:59:00 +0300

    Make Modules as Java Modules

---
+++ DMT-CORE/src/main/java/entity/Student.java
@@ -0,0 +1,8 @@
+package entity;
+
+/**
+ * @author mali.sahin
+ * @date 13-Mar-18
+ */
+public class Student {
+}
---
+++ DMT-CORE/src/main/java/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author mali.sahin
+ * @date 13-Mar-18
+ */
+@Repository
+public interface StudentRepository {
+}
