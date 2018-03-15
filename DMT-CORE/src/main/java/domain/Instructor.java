cc5ddd3a356c96fe3f2b82dc798661f4b735a5fe
yagmuravsar <yagmuravsar75@gmail.com>
2018-03-15 23:39:40 +0300

    Hello GitHub

--- DMT-CORE/src/main/java/domain/Instructor.java
+++ DMT-CORE/src/main/java/domain/Instructor.java
@@ -5,4 +5,7 @@ import java.io.Serializable;
 public class Instructor implements Serializable {
 
     String userName;
+
+    // biraz bekleticem
+    //tamam sorun var di mi:(( sana  yazma hakki vermem lazim sanirim.hııı mantıklı
 }
