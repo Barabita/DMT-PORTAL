7ea6a317ebf4e19d3d23123d28871ef1d27b3601
malisahin <mehmetalisahinogullari@gmail.com>
2018-03-21 20:35:38 +0300

    Spring Data Jpa Configurations

--- DMT-CORE/src/main/java/com/dmt/core/domain/Instructor.java
+++ DMT-CORE/src/main/java/com/dmt/core/domain/Instructor.java
@@ -1,45 +1,43 @@
 package com.dmt.core.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
+
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
+@Table(name = "INSTRUCTOR_DEF")
 public class Instructor implements Serializable {
 
 
-  /*  @Column(name = "ISIM", length = 150, nullable = false)
+    @Column(name = "ISIM", length = 150, nullable = false)
     String isim;
 
-    @Column(name = "ID" , length = 11, nullable = false)
-    int id;
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
 
-    @Column(name = "SURNAME" ,length = 150 , nullable = false )
+    @Column(name = "SURNAME", length = 150, nullable = false)
     String surname;
 
-    @Column (name = "NATIONAL_ID" ,length = 11, nullable = false )
+    @Column(name = "NATIONAL_ID", length = 11, nullable = false)
     int nationalId;
 
-    @Column(name = "ROOM_NO" ,length = 11)
+    @Column(name = "ROOM_NO", length = 11)
     int roomNo;
 
-    @Column(name="GSM_NO" ,length = 11)
+    @Column(name = "GSM_NO", length = 11)
     int gsmNo;
 
-    @Column(name="INTERNAL_NO" ,length = 11)
+    @Column(name = "INTERNAL_NO", length = 11)
     int internalNo;
 
-    @Column(name="EMAIL" ,length = 200)
+    @Column(name = "EMAIL", length = 200)
     String email;
 
-    @Column(name="WEEKLY_LECTURE_RES" ,length = 11)
+    @Column(name = "WEEKLY_LECTURE_RES", length = 11)
     int weeklyLectureRes;
 
-    @Column(name="PLEASURE" ,length = 11)
+    @Column(name = "PLEASURE", length = 11)
     int pleasure;
-
-*/
-
-
 
 }
---
+++ DMT-WEB/src/main/java/com/dmt/web/WebAppConfig.java
@@ -0,0 +1,50 @@
+package com.dmt.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+/**
+ * @author mali.sahin
+ * @date 21-Mar-18
+ */
+@Configuration
+@EnableTransactionManagement
+@ComponentScan("com.dmt")
+@PropertySource("classpath:application.properties")
+public class WebAppConfig {
+
+    private static final String PROPERTY_NAME_DATABASE_CLASS_NAME = "spring.datasource.driverClassName";
+    private static final String PROPERTY_NAME_DATABASE_PASSWORD = "spring.datasource.password";
+    private static final String PROPERTY_NAME_DATABASE_URL = "spring.datasource.url";
+    private static final String PROPERTY_NAME_DATABASE_USERNAME = "spring.datasource.username";
+    private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
+    private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
+    private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan";
+
+
+    @Resource
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getRequiredProperty(PROPERTY_NAME_DATABASE_CLASS_NAME));
+        dataSource.setUrl(env.getRequiredProperty(PROPERTY_NAME_DATABASE_URL));
+        dataSource.setUsername(env.getRequiredProperty(PROPERTY_NAME_DATABASE_USERNAME));
+        dataSource.setPassword(env.getRequiredProperty(PROPERTY_NAME_DATABASE_PASSWORD));
+        return dataSource;
+    }
+
+
+
+}
