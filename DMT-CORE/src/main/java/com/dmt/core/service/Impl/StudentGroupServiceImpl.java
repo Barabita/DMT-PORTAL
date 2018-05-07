11f8e5a490cdab53b3594a5ad3123dfd4ffd48ff
yagmuravsar <yagmuravsar75@gmail.com>
2018-05-08 00:00:26 +0300

    arrangement for leftAccordionMenu page and studentGroupServiceImpl page

--- DMT-CORE/src/main/java/com/dmt/core/service/Impl/StudentGroupServiceImpl.java
+++ DMT-CORE/src/main/java/com/dmt/core/service/Impl/StudentGroupServiceImpl.java
@@ -1,8 +1,12 @@
 package com.dmt.core.service.Impl;
 
+import com.dmt.core.domain.StudentGroup;
 import com.dmt.core.repository.StudentGroupRepository;
+import com.dmt.core.service.Search.SearchStudentGroup;
 import com.dmt.core.service.StudentGroupService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,4 +19,19 @@ public class StudentGroupServiceImpl implements StudentGroupService {
 
     @Autowired
     private StudentGroupRepository studentGroupRepository;
+
+    @Override
+    public StudentGroup save(StudentGroup group) {
+        return null;
+    }
+
+    @Override
+    public Page<StudentGroup> getStudentGroupList(SearchStudentGroup filter, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public void delete(StudentGroup student) {
+
+    }
 }
