d9b06bacdcf4d3465639b3f443f70082ad1c0b5c
yagmuravsar <yagmuravsar75@gmail.com>
2018-05-10 00:21:56 +0300

    Occur pageForLecture page

--- DMT-CORE/src/main/java/com/dmt/core/service/Impl/PlaceServiceImpl.java
+++ DMT-CORE/src/main/java/com/dmt/core/service/Impl/PlaceServiceImpl.java
@@ -1,9 +1,15 @@
 package com.dmt.core.service.Impl;
 
+import com.dmt.core.domain.enums.PlaceType;
 import com.dmt.core.repository.PlaceRepository;
 import com.dmt.core.service.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import javax.faces.model.SelectItem;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author: mali.sahin on 26.03.2018.
@@ -14,4 +20,13 @@ public class PlaceServiceImpl  implements PlaceService{
 
     @Autowired
     private PlaceRepository placeRepository;
+//
+//    @Override
+//    public List<SelectItem> getPlaceTypeList() {
+//        List<SelectItem> typeList = new ArrayList<>();
+//        typeList.add(new SelectItem(PlaceType.AMFI,"AMFİ"));
+//        typeList.add(new SelectItem(PlaceType.SINIF,"SINIF"));
+//        typeList.add(new SelectItem(PlaceType.LAB,"LABORATUVAR"));
+//        return typeList;
+    //}
 }
--- DMT-CORE/src/main/java/com/dmt/core/service/PlaceService.java
+++ DMT-CORE/src/main/java/com/dmt/core/service/PlaceService.java
@@ -1,9 +1,13 @@
 package com.dmt.core.service;
 
+
+
 /**
  * @author: mali.sahin on 26.03.2018.
  */
 
 
 public interface PlaceService {
+
+
 }
